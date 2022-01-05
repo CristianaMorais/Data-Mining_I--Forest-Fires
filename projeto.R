2be2d5fe94e7e97b036a783445bf8277c20a287a
Cristiana Morais <up201505454@fc.up.pt>
2022-01-05 17:12:10 +0000

    Update projeto.R

--- projeto.R
+++ projeto.R
@@ -3,11 +3,12 @@ library(dplyr)
 library(tidyverse)
 library(dlookr)
 
-fires_train <- read_csv("fires_train.csv")
-fires_test <- read_csv("fires_test.csv")
+fires_train <- read_csv("fires_train.csv", na= c("NA","", "-"), col_names = TRUE)
+fires_test <- read_csv("fires_test.csv", na= c("NA","", "-"), col_names = TRUE)
 
 
 # Task 1: Data importation, clean-up and pre-processing
+# Data clean-up and pre-processing steps.
 
 
 # Task 2: Data exploratory analysis
